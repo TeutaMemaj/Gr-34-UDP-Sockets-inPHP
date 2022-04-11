a5ecc551708ea1b09f2300d86f080f396c7d2a6c
Sumeja Veseli <94646360+SumejaVeseli@users.noreply.github.com>
2022-04-11 21:38:08 +0200

    Update client.php

--- client.php
+++ client.php
@@ -1,3 +1,17 @@
+<?php
+
+error_reporting(~E_WARNING);
+
+$server = '127.0.0.1';
+$port = 9999;
+if(!($sock = socket_create(AF_INET, SOCK_DGRAM, 0 )))
+{
+    $ERRORCODE = socket_last_error();
+    $errormsg = socket_strerror($errorcode);
+}
+
+
+
 die("Couldn't create socket: [$errorcode] $errormsg \n");
 
 }
